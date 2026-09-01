d82c4c713026e3a920cd89efb56288d4f32f3e37
nicobasa <142696216+nicobasa@users.noreply.github.com>
2026-08-31 22:29:50 -0300

    Agrego el dao de la clase UnidadVenta

---
+++ TrabajoGrupo5Oo2/src/dao/UnidadVentaDao.java
@@ -0,0 +1,89 @@
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import datos.UnidadVenta;
+
+public class UnidadVentaDao {
+
+    private static Session session;
+    private Transaction tx;
+
+    private void iniciaOperacion() throws HibernateException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        tx = session.beginTransaction();
+    }
+
+    private void manejaExcepcion(HibernateException he) throws HibernateException {
+        tx.rollback();
+        throw new HibernateException("ERROR en la capa de acceso a datos", he);
+    }
+
+    public int agregar(UnidadVenta objeto) {
+        int id = 0;
+        try {
+            iniciaOperacion();
+            id = Integer.parseInt(session.save(objeto).toString());
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+        } finally {
+            session.close();
+        }
+        return id;
+    }
+
+    public void actualizar(UnidadVenta objeto) {
+        try {
+            iniciaOperacion();
+            session.update(objeto);
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+        } finally {
+            session.close();
+        }
+    }
+
+    public void eliminar(UnidadVenta objeto) {
+        try {
+            iniciaOperacion();
+            session.delete(objeto);
+            tx.commit();
+        } catch (HibernateException he) {
+            manejaExcepcion(he);
+        } finally {
+            session.close();
+        }
+    }
+
+    public UnidadVenta traer(int idUnidadVenta) {
+        UnidadVenta objeto = null;
+        try {
+            iniciaOperacion();
+            objeto = (UnidadVenta) session.get(UnidadVenta.class, idUnidadVenta);
+        } finally {
+            session.close();
+        }
+        return objeto;
+    }
+
+    public List<UnidadVenta> traer() {
+        List<UnidadVenta> lista = new ArrayList<UnidadVenta>();
+        try {
+            iniciaOperacion();
+            Query<UnidadVenta> query = session.createQuery("from UnidadVenta u", UnidadVenta.class);
+            lista = query.getResultList();
+        } finally {
+            session.close();
+        }
+        return lista;
+    }
+
+}
